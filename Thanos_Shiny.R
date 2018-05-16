1375b0388c41c821550abcc46ff5bbba07f7c1c1
doughertyeric <dougherty.eric@gmail.com>
2018-05-16 17:35:16 -0400

    Initial Shiny App with histogram

---
+++ Thanos_Shiny.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+library(ggmap)
+library(shiny)
+
+towns <- read_csv('Towns_Population_2016.csv') %>%
+  filter(PLACE != 0) %>%
+  filter(PLACE != 99990) %>%
+  filter(POPESTIMATE2016 != 0)
+towns$lon <- NA
+towns$lat <- NA
+
+# Create a name to use for searching the Google Maps API
+name_list <- c()
+for (i in 1:nrow(towns)) {
+  name <- towns$NAME[i]
+  name <- gsub(x=name, pattern=' city', '', ignore.case=FALSE)
+  name <- gsub(x=name, pattern=' town', '', ignore.case=FALSE)
+  name_list[i] <- paste0(name, ", ", towns$STNAME[i])
+}
+
+# Use the geocode command to return the coordinates of each town
+for (i in 1:length(name_list)) {
+  temp.coords <- geocode(name_list[i], force=TRUE, override_limit=TRUE)
+  towns$lon[i] <- temp.coords$lon
+  towns$lat[i] <- temp.coords$lat
+}
+
+print(max(all_zeros$Population))
+
+all_zeros <- st_as_sf(all_zeros, coords=4:5, crs=4326)
+max_zero <- all_zeros[1,]
+
+ggplot() + geom_sf(data=all_zeros, aes(size=Population, fill=1))
+
+
+################################################
+
+ui <- fluidPage(
+  sliderInput(inputId = 'depart.prob',
+              label = 'Departure Probability',
+              value = 0.5, min = 0.01, max = 0.99),
+  actionButton("rerun", "Re-run Simulation"),
+  plotOutput(outputId = 'map')
+)
+
+server <- function(input, output) {
+  
+  all_zeros <- eventReactive(input$rerun, {
+    departure.sim <- function(depart.prob) {
+      zero_departures <- c()
+      z=1
+      for (i in 1:nrow(towns)) {
+        all_draws <- rbinom(n=1, size=towns$POPESTIMATE2016[i], p=depart.prob)
+        if (sum(all_draws) == 0) {
+          zero_departures[z] <- i
+          z = z + 1
+        }
+      }
+      
+      all_zeros <- data.frame(matrix(0,length(zero_departures),5))
+      colnames(all_zeros) <- c('Town', 'State', 'Population', 'lon', 'lat')
+      for (j in 1:length(zero_departures)) {
+        row.id <- zero_departures[j]
+        all_zeros[j,] <- towns[row.id, c('NAME', 'STNAME', 'POPESTIMATE2016', 'lon', 'lat')]
+      }
+      all_zeros <- all_zeros[order(all_zeros$Population, decreasing=TRUE),]
+      
+      return(all_zeros)
+    }
+    
+    departure.sim(input$depart.prob)
+  })
+  
+  output$map <- renderPlot({
+    all_zeros <- all_zeros()
+    ggplot(data=all_zeros) + geom_histogram(aes(x=Population), binwidth=1)
+  })
+}
+
+shinyApp(ui = ui, server = server)
